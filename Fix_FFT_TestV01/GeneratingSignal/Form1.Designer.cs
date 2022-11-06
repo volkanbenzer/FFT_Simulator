042104e9f9e110612b4bca4081f268b36b1ee277
Volkan Benzer <47257339+volkanbenzer@users.noreply.github.com>
2022-11-07 00:02:00 +0300

    First commit

---
+++ Fix_FFT_TestV01/GeneratingSignal/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace GeneratingSignal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(9, 9);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Continuous";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Sampled";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(663, 190);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart3
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart3.Legends.Add(legend2);
+            this.chart3.Location = new System.Drawing.Point(8, 204);
+            this.chart3.Margin = new System.Windows.Forms.Padding(2);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Spectrum";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(944, 196);
+            this.chart3.TabIndex = 6;
+            this.chart3.Text = "chart3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(691, 114);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Freq Band: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(731, 5);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "10000";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(677, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Max Freq";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1000;
+            this.trackBar1.Location = new System.Drawing.Point(685, 154);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar1.Maximum = 10000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(268, 45);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.TickFrequency = 100;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(731, 26);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 20);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(709, 28);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Fs";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(731, 51);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(68, 20);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(699, 51);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Amp";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(872, 90);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Theta: 0";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(875, 105);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(75, 45);
+            this.trackBar2.TabIndex = 16;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(691, 137);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Freq: 0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(819, 9);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "LOG2_FFT_SIZE";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(918, 7);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDown1.TabIndex = 20;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(819, 29);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "FFT Size: ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(819, 51);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(86, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "FREQ_RANGE: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 401);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Fix_FFT_TestV01/GeneratingSignal/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+//Created By Volkan Benzer
+//https://www.linkedin.com/in/volkan-benzer-26tr34ist/
+
+
+namespace GeneratingSignal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double TestWave_Freq = 5000;
+        double TestWave_Theta = (float)(Math.PI / 2.0);
+        double TestWave_Fsample = 20000;
+        double TestWave_Amplitude = 3.0f;
+
+        int TestWave_SampleCount;
+
+        ushort LOG2_FFT_SIZE = 6;
+        ushort FFT_SIZE;
+        float FREQUENCY_RANGE;
+ 
+        double[] sineWave;
+        double[] sampleBufferInDouble;
+        short[]  sampleBufferInShort;
+
+        fix_FFT _fix_FFT = new fix_FFT();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //to preventing of start the X axes from -1 
+            chart1.ChartAreas[0].AxisX.Minimum = 0;            
+            chart3.ChartAreas[0].AxisX.Minimum = 0;
+
+            //chart3.ChartAreas[0].AxisX.Minimum = 0;
+            //or    (dezavantajı, hep sıfırdan başlatıyor. örn işaret 5. saniyeden başlaması gerekiyorsa hatalı oluyor)
+            //chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
+            //chart2.ChartAreas[0].AxisX.IsMarginVisible = false;
+
+            chart3.ChartAreas[0].AxisX.LabelStyle.Angle = 45;
+
+            trackBar2.Minimum = 0;
+            trackBar2.Maximum = 16;
+
+            trackBar1.Value = 0;
+            trackBar2.Value = 0;
+
+            numericUpDown1.Value = 5;
+
+            textBox1.Text = "10000";
+            textBox2.Text = "20000";
+            textBox3.Text = "3.0";
+
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = Convert.ToInt16(textBox1.Text);
+        }
+
+        void generateSineWave(double freq, double amplitude, double timeResulation, double theta, double periodCount)
+        {
+            //y(t) = Asin(2πft + ϕ) = Asin(ωt + ϕ)
+            // t => n
+
+            freq = freq / timeResulation;        //freq in timeResulation Time                         
+            double _1cyclePeriode = (1.0 / freq);
+            double w = (2.0 * Math.PI * freq);
+
+            int signalLenght = (int)(_1cyclePeriode * periodCount);
+
+            sineWave = new double[signalLenght];
+
+            for (int n = 0; n < signalLenght; n++)
+            {
+                sineWave[n] = amplitude * Math.Sin(w * (double)n + theta);
+
+                chart1.Series["Continuous"].Points.AddXY(n, sineWave[n]);
+            }            
+
+            //label1.Text = _1cyclePeriode.ToString();
+        }
+
+        void getSamples(double[] signalBuffer, double fs, int sampleCount)
+        {
+            
+            double samplePeriod = fs;//(int)((1.0 / fs) * timeResulation);   //Fs is converted to period, period is also converted in timeResulation
+
+            sampleBufferInDouble = new double[sampleCount];
+
+            for (int n = 0; n < sampleCount; n++)
+            {
+                sampleBufferInDouble[n] = signalBuffer[n + 1];  //we dont want to get first sample in t = 0
+
+                chart1.Series["Sampled"].Points.AddXY(n + 1, sampleBufferInDouble[n]);
+            }
+        }
+
+        void convertSamplesToShort(double[] sampleBuffer, double amplitude)
+        {
+            sampleBufferInShort = new short[sampleBuffer.Length];
+
+            for (int n = 0; n < sampleBuffer.Length; n++)            
+                sampleBufferInShort[n] = (short)((sampleBuffer[n] * 32767f) / amplitude);            
+        }
+
+        void test_FixFFT()
+        {            
+            
+            CreateTestSignal(TestWave_Freq, TestWave_Theta, TestWave_Fsample, TestWave_Amplitude, TestWave_SampleCount);
+
+            short[] real = new short[FFT_SIZE];
+            short[] imaginary = new short[FFT_SIZE];
+            int[] output = new int[FFT_SIZE >> 1];
+            int max_Val = 0, max_Index = 0;
+
+
+            float[] freqBand = new float[FFT_SIZE >> 1];
+
+            for (int i = 0; i < FFT_SIZE; i++)
+            {
+                real[i] = sampleBufferInShort[i];
+                imaginary[i] = 0;
+            }
+
+            _fix_FFT.fix_fft(real, imaginary, (short)LOG2_FFT_SIZE, 0);
+
+            for (int i = 0; i < (FFT_SIZE >> 1); i++)  // Make frequency range table 	
+                freqBand[i] = FREQUENCY_RANGE * i;
+
+            chart3.ChartAreas[0].AxisX.Interval = FREQUENCY_RANGE;
+
+            for (int i = 0; i < (FFT_SIZE >> 1); i++)
+            {
+                output[i] = (int)Math.Sqrt(Math.Abs(_fix_FFT.real[i]) + Math.Abs(_fix_FFT.imaginary[i]));
+
+                if (max_Val < output[i])
+                {
+                    max_Val = output[i];
+                    max_Index = i;   
+                }
+            }
+
+            chart3.Series["Spectrum"].Points.DataBindXY(freqBand, output);
+            
+            chart3.ChartAreas[0].AxisX.LabelStyle.Angle = 45;
+
+            label2.Text = "Freq Band: " + freqBand[max_Index].ToString() + " - " + (freqBand[max_Index] + FREQUENCY_RANGE).ToString();
+        }
+
+        void CreateTestSignal(double TestWave_Freq, double TestWave_Theta, double TestWave_Fsample, double TestWave_Amplitude, int TestWave_SampleCount)
+        {
+            double minPeriodCount = ((double)TestWave_SampleCount / ((double)TestWave_Fsample / (double)TestWave_Freq)) + 1;
+            // You must create sufficiently long signal for sufficiently sampling. because of + 1 is that sampling doesn't start from t = 0.
+            
+            generateSineWave(TestWave_Freq, TestWave_Amplitude, TestWave_Fsample, TestWave_Theta, minPeriodCount);
+
+            getSamples(sineWave, TestWave_Fsample, TestWave_SampleCount);
+
+            convertSamplesToShort(sampleBufferInDouble, TestWave_Amplitude);
+        }   
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            TestWave_Freq = trackBar1.Value;
+            label7.Text = "Freq: " + TestWave_Freq.ToString();
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            test_FixFFT();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            TestWave_Theta = (180/ Math.PI) * (float)((Math.PI / (float)trackBar2.Maximum) * (float)(trackBar2.Value));
+            label6.Text = "Theta: " + TestWave_Theta.ToString("##.##");
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            test_FixFFT();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                TestWave_Fsample = Convert.ToDouble(textBox2.Text);
+                FREQUENCY_RANGE = ((float)TestWave_Fsample / 2) / (FFT_SIZE / 2);
+            }
+            catch
+            {
+                FREQUENCY_RANGE = 0;
+            }
+
+            label10.Text = "FREQ_RANGE: "  + FREQUENCY_RANGE.ToString(); 
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            LOG2_FFT_SIZE = (ushort)numericUpDown1.Value;
+            FFT_SIZE = (ushort)Math.Pow(2, LOG2_FFT_SIZE);
+            label9.Text = "FFT Size: " + FFT_SIZE.ToString();
+
+            TestWave_SampleCount = FFT_SIZE;
+
+            textBox2_TextChanged(sender, e);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            TestWave_Amplitude = Convert.ToDouble(textBox3.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            trackBar1.Maximum = Convert.ToInt16(textBox1.Text);
+        }
+    }
+}
